954530ff70e80edb0a01e7118be148eb54181c27
dttung79 <dttung@gmail.com>
2026-06-25 21:44:00 +0700

    demo audio

---
+++ day2/tut05_txt_audio.py
@@ -0,0 +1,38 @@
+from google import genai
+from google.genai import types
+import wave
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from utils import get_key
+
+# Set up the wave file to save the output:
+def wave_file(filename, pcm, channels=1, rate=24000, sample_width=2):
+   with wave.open(filename, "wb") as wf:
+      wf.setnchannels(channels)
+      wf.setsampwidth(sample_width)
+      wf.setframerate(rate)
+      wf.writeframes(pcm)
+
+client = genai.Client(api_key=get_key())
+
+response = client.models.generate_content(
+   model="gemini-2.5-flash-preview-tts",
+   contents=input("Enter text to convert to speech: "),
+   config=types.GenerateContentConfig(
+      response_modalities=["AUDIO"],
+      speech_config=types.SpeechConfig(
+         voice_config=types.VoiceConfig(
+            prebuilt_voice_config=types.PrebuiltVoiceConfig(
+               voice_name='Orus',
+            )
+         )
+      ),
+   )
+)
+
+data = response.candidates[0].content.parts[0].inline_data.data
+
+file_name='out.wav'
+wave_file(file_name, data) # Saves the file to current directory
